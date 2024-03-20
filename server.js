c9b54a0633f7b1270b9ae6e095189ef30f09a710
Takeru Kakuta <kakuta19960915@gmail.com>
2024-03-20 17:33:05 +0900

    ✨ feat: フォームをクリックした時の機能

--- server.js
+++ server.js
@@ -2,6 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// シークレットキー
+const stripe = require('stripe')('sk_test_51OwJ4iKhKNkDMmE5r76y79tcC6ZRlb3xiWVBK25mC67XaRieiskJaZb0gdpqOm2xSj7FzeTGlMTt1VJxZrLX9cAS00RE3jMvzE');
+
+const YOUR_DOMAIN = "localhost:3000";
+
 app.use(express.static("public"));
+
+// フォームボタンを押すとPOSTメソッド実行
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    // pricesにサブスクの情報が入っている
+    const prices = await stripe.prices.list();
+
+    // sessionを作成 (何を購入したのか、購入した量などの情報)
+    const session = await stripe.checkout.create({
+      line_items: [{
+        price: prices.data[0].id,
+        quantity: 1,
+      }],
+      mode: "subscription",
+      // どこに飛ぶかURLを指定
+      success_url: `${YOUR_DOMAIN}/sccess.html?session_id{CHECKOUT_SESSION_ID}`,
+    })
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(PORT, console.log("サーバーが起動しました"));
